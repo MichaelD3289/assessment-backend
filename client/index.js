623a8044276380943e56bcd746232b3717121ab3
Michael Drummond <michael.drummond328@gmail.com>
2022-02-11 09:22:43 -0700

    file restructuring

---
+++ client/index.js
@@ -0,0 +1,8 @@
+document.getElementById("complimentButton").onclick = 
+ () => {
+  axios.get("http://localhost:4000/api/compliment/")
+      .then((response) => {
+        const data = response.data;
+        alert(data);
+      });
+};
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const cntrl = require('./controller')
 
 const app = express();
 
@@ -8,18 +9,6 @@ app.use(cors());
 
 app.use(express.json()); // When we want to be able to accept JSON.
 
-app.get("/api/compliment", (req, res) => {
-  const compliments = ["Gee, you're a smart cookie!",
-					 "Cool shirt!",
-					 "Your Javascript skills are stellar.",
-  ];
-
-  // choose random compliment
-  let randomIndex = Math.floor(Math.random() * compliments.length);
-  let randomCompliment = compliments[randomIndex];
-
-  res.status(200).send(randomCompliment);
-  
-});
+app.get("/api/compliment", cntrl.getCompliment);
 
 app.listen(4000, () => console.log("Server running on 4000"));
